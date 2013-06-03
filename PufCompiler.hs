72e2ff6e89fc870d61825f9ba2f70991bc3a722b
Karl Tarbe <karulont@gmail.com>
2013-06-03 15:26:01 +0300

    Little improvements.

--- PufCompiler.hs
+++ PufCompiler.hs
@@ -5,4 +5,68 @@ import PufCompilerTypes
 import Control.Monad.State
 
 compileAST :: AST String -> String
-compileAST = undefined
+compileAST ast  = pprint (codegen ast)
+
+pprint :: InstList -> String
+pprint (Cat il1 il2) = pprint il1 ++ ('\n' : pprint il2)
+pprint i = printInstruction i
+
+
+codegen ast = evalState (codeV ast) initialState <> Halt
+
+getNextLabel = do (CompilerState e s l) <- get
+                  put (CompilerState e s (l+1))
+                  return l
+
+codeB :: AST String -> State CompilerState InstList
+codeV :: AST String -> State CompilerState InstList 
+codeC :: AST String -> State CompilerState InstList 
+
+codeB (Num i) = return (Loadc i)
+
+codeB (App (Prim op) e) = do exp <- codeB e 
+                             return (exp <> builtinInst op)
+
+codeB (App (App (Prim op) e1) e2) = do exp1 <- codeB e1
+                                       modify (modSD 1)
+                                       exp2 <- codeB e2
+                                       return (exp1 <> exp2)
+
+codeB (Cond cond e1 e2) = do cxp <- codeB cond
+                             la  <- getNextLabel
+                             ex1 <- codeB e1
+                             lb  <- getNextLabel
+                             ex2 <- codeB e2
+                             return (cxp 
+                                 <> Jumpz la
+                                 <> ex1
+                                 <> Jump lb
+                                 <> Label la
+                                 <> ex2
+                                 <> Label lb)
+
+
+
+codeV (Num i) = return (Loadc i <> Mkbasic)
+codeV ex@(App (Prim _) _) = do exp <- codeB ex 
+                               return (exp <> Mkbasic)
+
+codeV ex@(App (App (Prim _) _) _) = do exp <- codeB ex
+                                       return (exp <> Mkbasic)
+
+codeV (Cond cond e1 e2) = do cxp <- codeB cond
+                             la  <- getNextLabel
+                             ex1 <- codeV e1
+                             lb  <- getNextLabel
+                             ex2 <- codeV e2
+                             return (cxp 
+                                 <> Jumpz la
+                                 <> ex1
+                                 <> Jump lb
+                                 <> Label la
+                                 <> ex2
+                                 <> Label lb)
+
+--codeV (Rec rdecls asd) = 
+
+codeC = undefined
--- PufCompilerTypes.hs
+++ PufCompilerTypes.hs
@@ -1,3 +1,89 @@
 module PufCompilerTypes where
 
+import PufAST
 
+data Instruction
+     = Mkbasic
+     | Getbasic
+     | Eval
+     | Add
+     | Sub
+     | Mul
+     | Div
+     | Leq
+     | Eq
+     | Apply
+     | Update
+     | Halt
+     | Neg
+     | Not
+     | Mod
+     | Neq
+     | Le
+     | Geq
+     | Gr
+     | Or
+     | And
+     | Nil
+     | Cons
+     | Loadc Int
+     | Pushloc Int
+     | Pushglob Int
+     | Targ Int
+     | Return Int
+     | Slide Int
+     | Alloc Int
+     | Rewrite Int
+     | Mkvec Int
+     | Getvec Int
+     | Get Int
+     | Mkclos Int
+     | Mkfunval Int
+     | Mark Int
+     | Jumpz Int
+     | Jump Int
+     | Tlist Int
+     | Move Int Int
+     | Label Int
+     | Cat Instruction Instruction
+     deriving (Show)
+
+type InstList = Instruction
+
+
+data CompilerState = CompilerState [(Char,Int)] Int Int
+                     deriving (Show)
+
+initialState = CompilerState [] 0 0
+
+modSD :: Int -> CompilerState -> CompilerState
+modSD x (CompilerState e sd l) = CompilerState e (sd+x) l
+
+infixr 6 <>
+
+(<>) :: InstList -> InstList -> InstList
+x <> y = Cat x y
+                                    
+printInstruction :: Instruction -> String
+printInstruction x = case x of
+                       Mkbasic -> "mkbasic"
+                       Getbasic -> "getbasic"
+                       Label i -> "\t_" ++ show i
+                       _ -> ""
+
+builtinInst :: Builtin -> Instruction
+builtinInst UNeg = Neg
+builtinInst UNot = Not
+builtinInst BAdd = Add
+builtinInst BSub = Sub
+builtinInst BMul = Mul
+builtinInst BDiv = Div
+builtinInst BMod = Mod
+builtinInst BEq = Eq
+builtinInst BNe = Neq
+builtinInst BLe = Leq
+builtinInst BLt = Le
+builtinInst BGe = Geq
+builtinInst BGt = Gr
+builtinInst BOr = Or
+builtinInst BAnd = And
